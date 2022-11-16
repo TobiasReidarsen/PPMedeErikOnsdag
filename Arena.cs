f274f7e6047f57921eaf90b390379d6e958e3a11
WinstonSmith314 <tobias.reidarsen@gmail.com>
2022-11-16 13:36:23 +0100

    asdasd

--- Arena.cs
+++ Arena.cs
@@ -3,18 +3,16 @@
     internal class Arena //: IFighterClass
     {
         private List<IFighterClass> Fighters;
-        private List<IFighterClass> Slåsskjemper;
+
 
 
         public Arena()
         {
             Fighters = new List<IFighterClass>();
-            Fighters.Add(new BossClass("Thomas The Daddy", 1234, 1234, 12, 32));
-            Fighters.Add(new HeroClass("Erik", 2, 2, 1, 0));
+            Fighters.Add(new BossClass("Thomas The Daddy", 500, 20, 60, 32));
+            Fighters.Add(new HeroClass("Erik", 250, 65, 60, 0));
 
-            Slåsskjemper = new List<IFighterClass>();
-            Slåsskjemper.Add(new BossClass("Thomas The Daddy", 1234, 1234, 12, 32));
-            Slåsskjemper.Add(new HeroClass("Tobias", 2, 2, 1, 13));
+
 
 
         }
@@ -25,11 +23,7 @@
                 fighter.SayName();
 
             }
-            foreach (var fighter in Slåsskjemper)
-            {
-                fighter.SayName();
 
-            }
         }
 
         public void fight()
